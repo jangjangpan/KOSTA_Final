5501c317c62e07eff342abae83bcc0d7f5997c8c
guest1216 <moonmoth1623@gmail.com>
2021-06-03 11:18:28 +0900

    스프링부트 프로젝트 추가

---
+++ src/test/java/com/kosta/sbproject/KostaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.sbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KostaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
