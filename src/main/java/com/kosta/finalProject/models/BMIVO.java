102c5b535acc0ef0a9c5848fe2de982ebc0004ce
ohjungs <81200313+ohjungs@users.noreply.github.com>
2021-06-07 14:20:22 +0900

    오정훈

--- src/main/java/com/kosta/finalProject/models/BMIVO.java
+++ src/main/java/com/kosta/finalProject/models/BMIVO.java
@@ -2,17 +2,26 @@ package com.kosta.finalProject.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "BMI_TB")
 public class BMIVO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="Seoul_district")
-	String district;
-	
+	String district;	
 	
 	float under_weight;
 	float normal_weight;
